743b535b1165a3f97a8e32d4eaeca21807024c0f
Srini K <srinikdevops0131@gmail.com>
2023-04-03 13:56:27 -0700

    updating ./roboshop-jenkins

--- provider.tf
+++ provider.tf
@@ -1,8 +1,22 @@
-provider "jenkins" {
-  server_url = "http://172.31.3.4:8080"
-  username   = "username"
-  password   = "password"
-  ca_cert = ""
+terraform {
+  required_providers {
+    jenkins = {
+      source = "registry.terraform.io/taiidani/jenkins"
+    }
+  }
 }
 
-data 
+provider "jenkins" {
+  server_url = "http://172.31.3.4:8080"
+  username   = data.aws_ssm_parameter.jenkins_user.value
+  password   = data.aws_ssm_parameter.jenkins_pass.value
+}
+
+data "aws_ssm_parameter" "jenkins_user"{
+  name = "jenkins.user"
+}
+
+
+data "aws_ssm_parameter" "jenkins_pass"{
+  name = "jenkins.pass"
+}
