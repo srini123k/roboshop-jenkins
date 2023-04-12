c491021396cf807e43afde0cb07d942dc3b2d70b
Srini K <srinikdevops0131@gmail.com>
2023-04-12 08:36:39 -0700

    updating ./roboshop-jenkins

--- vars.tf
+++ vars.tf
@@ -3,16 +3,21 @@ variable "folders" {
 
 }
 
-variable "jobs" {
+variable "m-jobs" {
   default = [
 
-    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/srini123k/roboshop-infra" },
     { name = "frontend", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/frontend" },
     { name = "cart", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/cart" },
     { name = "catalogue", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/catalogue" },
     { name = "user", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/user" },
     { name = "shipping", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/shipping" },
     { name = "payment", folder = "CI-Pipeline", repo_url = "https://github.com/srini123k/payment" }
+  ]
+}
+
+variable "s-jobs" {
+  default = [
+    { name = "roboshop", folder = "Infra", repo_url = "https://github.com/raghudevopsb71/roboshop-infra" }
   ]
 }
 
